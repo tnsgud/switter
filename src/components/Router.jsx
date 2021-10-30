f9a836461ecc738dc93fac914d4fc7917e343f6d
tnsgud <qkrtnsgud0229@gmail.com>
2021-10-30 23:30:52 +0900

    putting action (edit profile)

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -7,7 +7,7 @@ import Profile from 'routes/Profile';
 const AppRouter = ({ isLoggedIn, userObj }) => {
     return (
         <Router>
-            {isLoggedIn && <Navigation />}
+            {isLoggedIn && <Navigation userObj={userObj} />}
             <Switch>
                 {isLoggedIn ? (
                     <>
@@ -15,7 +15,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
                             <Home userObj={userObj} />
                         </Route>
                         <Route exact path="/profile">
-                            <Profile />
+                            <Profile userObj={userObj} />
                         </Route>
                     </>
                 ) : (
--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,16 +1,56 @@
-import { authService } from 'fBase';
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
+import { authService, dbService } from 'fBase';
+import { collection, getDocs, query, where, orderBy } from '@firebase/firestore';
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
 
     const onLogOutClick = () => {
         authService.signOut();
         history.push('/');
     };
 
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewDisplayName(value);
+    };
+
+    const getMySweets = async () => {
+        const docQuery = query(
+            collection(dbService, 'sweet'),
+            where('creator', '==', userObj.uid),
+            orderBy('createAt', 'asc')
+        );
+        const sweets = await getDocs(docQuery);
+
+        sweets.docs.map((doc) => {
+            console.log(doc.data());
+        });
+    };
+
+    useEffect(() => {
+        getMySweets();
+    }, []);
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    placeholder="Display name"
+                    onChange={onChange}
+                    value={newDisplayName}
+                />
+                <input type="submit" value="Update Profile" />
+            </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
